96c26788adec60ddb0838868077d6cee5c8e599f
Jon Saad-Falcon <jonsaadfalcon@DN51s5iu.SUNet>
2024-04-12 00:12:48 -0700

    tokenizer code

--- testing_tokenizer.py
+++ testing_tokenizer.py
@@ -1,4 +1,4 @@
 
 from tests.adapters import run_train_bpe
 
-run_train_bpe("data/TinyStoriesV2-GPT4-train.txt", 10000, ["<|endoftext|> s"])
+run_train_bpe("data/TinyStoriesV2-GPT4-train.txt", 10000, ["<|endoftext|>"])
--- tests/adapters.py
+++ tests/adapters.py
@@ -799,6 +799,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
+    #raise NotImplementedError
     from tests.tokenizer import Tokenizer
     return Tokenizer(vocab, merges, special_tokens)
 
