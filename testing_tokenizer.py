ff0c7c47db1b51af02c9eaa97d7f3ff9cbf56bbf
Jon Saad-Falcon <jonsaadfalcon@DN51s5iu.SUNet>
2024-04-12 00:47:51 -0700

    tokenizer code

--- testing_tokenizer.py
+++ testing_tokenizer.py
@@ -1,5 +1,19 @@
 
 from tests.adapters import run_train_bpe
+import json
+import os
 
-breakpoint()
-run_train_bpe("data/TinyStoriesV2-GPT4-train.txt", 10000, ["<|endoftext|>"])
+dataset = "TinyStoriesV2-GPT4-train" #"owt_train"
+
+if not os.path.exists(f"data/{dataset}"):
+    os.makedirs(f"data/{dataset}")
+
+vocabulary, merges_overall = run_train_bpe(f"data/{dataset}.txt", 10000, ["<|endoftext|>"])
+    
+# Save vocabulary to json file
+with open(f"data/{dataset}vocabulary.json", "w") as f:
+    json.dump(vocabulary, f)
+
+# Save merges_overall to json file
+with open(f"data/{dataset}/merges_overall.json", "w") as f:
+    json.dump(merges_overall, f)
