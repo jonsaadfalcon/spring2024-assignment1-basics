4c06c983fa02688f992e06a7a57e4e80b03c8517
Jon Saad-Falcon <jonsaadfalcon@Jons-MacBook-Pro.local>
2024-04-13 13:20:25 -0700

    update

--- run_tokenizer.sh
+++ run_tokenizer.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=100G
-#SBATCH --time=8:00:00
-#SBATCH --output=hello_batch_v2_%j.out
-#SBATCH --error=hello_batch_v2_%j.err
+#SBATCH --time=6:00:00
+#SBATCH --output=hello_batch_v2.1_%j.out
+#SBATCH --error=hello_batch_v2.1_%j.err
 
 # Optional: activate a conda environment to use for this job
 # eval "$(conda shell.bash hook)"
