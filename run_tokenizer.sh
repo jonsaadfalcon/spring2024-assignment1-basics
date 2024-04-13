fba6b5b69a6a88efff0a6aa32a8d3887c23a3c98
Jon Saad-Falcon <jonsaadfalcon@Jons-MacBook-Pro.local>
2024-04-13 13:16:57 -0700

    update

--- run_tokenizer.sh
+++ run_tokenizer.sh
@@ -2,13 +2,14 @@
 #SBATCH --job-name=test_hello_batch
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=02:00:00
-#SBATCH --output=hello_batch_%j.out
-#SBATCH --error=hello_batch_%j.err
+#SBATCH --cpus-per-task=8
+#SBATCH --time=24:00:00
+#SBATCH --mem=30G
+#SBATCH --output=hello_batch_v2_%j.out
+#SBATCH --error=hello_batch_v2_%j.err
 
 # Optional: activate a conda environment to use for this job
 # eval "$(conda shell.bash hook)"
 # conda activate cs336_basics
 
-python3 testing_tokenizer.py
+nohup python3 testing_tokenizer.py &> logs/owl_training.log &
