f227733978d5fb813daa4547179b5fb4807562cb
Jon Saad-Falcon <jonsaadfalcon@DN51s5iu.SUNet>
2024-04-12 19:57:52 -0700

    update

--- testing_tokenizer.py
+++ testing_tokenizer.py
@@ -10,6 +10,8 @@ if not os.path.exists(f"data/{dataset}"):
 
 vocabulary, merges_overall = run_train_bpe(f"data/{dataset}.txt", 10000, ["<|endoftext|>"])
     
+breakpoint()
+
 # Save vocabulary to json file
 with open(f"data/{dataset}vocabulary.json", "w") as f:
     json.dump(vocabulary, f)
