2064cc114b62c460183ec79467eac823e2139c58
Jon Saad-Falcon <jonsaadfalcon@Jons-MacBook-Pro.local>
2024-04-13 13:25:00 -0700

    update

--- run_tokenizer.sh
+++ run_tokenizer.sh
@@ -5,11 +5,11 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=100G
 #SBATCH --time=6:00:00
-#SBATCH --output=hello_batch_v2.1_%j.out
-#SBATCH --error=hello_batch_v2.1_%j.err
+#SBATCH --output=outputs/hello_batch_v2.1_%j.out
+#SBATCH --error=outputs/hello_batch_v2.1_%j.err
 
 # Optional: activate a conda environment to use for this job
 # eval "$(conda shell.bash hook)"
 # conda activate cs336_basics
 
-nohup python3 testing_tokenizer.py &> logs/tiny-stories_training.log &
+python3 testing_tokenizer.py
