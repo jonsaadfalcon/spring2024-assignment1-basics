f46ac34fae7dda0a4d560ad4a2e2aa8ecdad5210
Jon Saad-Falcon <jonsaadfalcon@Jons-MacBook-Pro.local>
2024-04-13 13:20:02 -0700

    update

--- run_tokenizer.sh
+++ run_tokenizer.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=100G
-#SBATCH --time=12:00:00
+#SBATCH --time=8:00:00
 #SBATCH --output=hello_batch_v2_%j.out
 #SBATCH --error=hello_batch_v2_%j.err
 
